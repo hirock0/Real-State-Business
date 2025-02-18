7b1d91ff5992ff80357302ebc92d9893076c6ab5
hirock0 <hirockdutta0@gmail.com>
2025-02-18 18:53:01 +0600

    banner button created

--- b10a12-client-side-hirock0-main/src/components/Banner/Banner.jsx
+++ b10a12-client-side-hirock0-main/src/components/Banner/Banner.jsx
@@ -3,8 +3,19 @@ import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
+  const bannerImages = [
+    {
+      title: "Find Your Dream Home",
+      img: "https://cdn.pixabay.com/photo/2016/12/22/18/20/skyline-1925943_960_720.jpg",
+    },
+    {
+      title: "Luxary Home",
+      img: "https://cdn.pixabay.com/photo/2019/07/13/16/30/architecture-4335215_960_720.jpg",
+    },
+  ];
   return (
     <div className="">
       <Swiper
@@ -16,32 +27,27 @@ const Banner = () => {
         loop={true}
         modules={[Pagination, Navigation, Autoplay]}
       >
-        <SwiperSlide>
-          <section
-            className="h-[500px] bg-cover bg-center flex items-center justify-center"
-            style={{
-              backgroundImage:
-                "url('https://cdn.pixabay.com/photo/2016/12/22/18/20/skyline-1925943_960_720.jpg')",
-            }}
-          >
-            <h1 className="text-white text-4xl md:text-6xl font-bold">
-              Find Your Dream Home
-            </h1>
-          </section>
-        </SwiperSlide>
-        <SwiperSlide>
-          <section
-            className="h-[500px] bg-cover bg-center flex items-center justify-center"
-            style={{
-              backgroundImage:
-                "url('https://cdn.pixabay.com/photo/2019/07/13/16/30/architecture-4335215_960_720.jpg')",
-            }}
-          >
-            <h1 className="text-white text-4xl md:text-6xl font-bold">
-              Find Your Dream Home
-            </h1>
-          </section>
-        </SwiperSlide>
+        {bannerImages?.map((item, index) => (
+          <SwiperSlide key={index}>
+            <section
+              className="h-[500px] bg-cover bg-center flex flex-col items-center justify-center"
+              style={{
+                backgroundImage: `url(${item?.img})`,
+              }}
+            >
+              <h1 className="text-white text-4xl md:text-6xl font-bold">
+                {item?.title}
+              </h1>
+              <div className=" mt-5 flex items-center justify-center">
+                <Link to={""}>
+                  <button className="btn btn-primary shadow-md">
+                    See More
+                  </button>
+                </Link>
+              </div>
+            </section>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
